33425f73bf6ed02cdca2d76b10f6bffa034a66ec
itolog <paukanlove@gmail.com>
2024-05-15 21:42:07 +0300

    Changed hero controller

--- src/components/AppCanvas/AppCanvas.tsx
+++ src/components/AppCanvas/AppCanvas.tsx
@@ -2,11 +2,14 @@ import { Canvas } from "@react-three/fiber";
 import { CanvasProps } from "@react-three/fiber/dist/declarations/src/web/Canvas";
 import { FC, PropsWithChildren, Suspense } from "react";
 
+import { EcctrlJoystick } from "ecctrl";
+
 interface Props extends CanvasProps {}
 
 const AppCanvas: FC<PropsWithChildren<Props>> = ({ children }) => {
 	return (
 		<Suspense fallback={"...loading"}>
+			<EcctrlJoystick />
 			<Canvas shadows>{children}</Canvas>
 		</Suspense>
 	);
