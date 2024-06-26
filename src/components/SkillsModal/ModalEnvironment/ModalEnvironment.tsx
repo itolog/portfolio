f209a4ccce058025750fd668fee49f7b2539035c
itolog <paukanlove@gmail.com>
2024-06-26 17:34:54 +0300

    ModalEnvironment

---
+++ src/components/SkillsModal/ModalEnvironment/ModalEnvironment.tsx
@@ -0,0 +1,12 @@
+import { Environment } from "@react-three/drei";
+
+const ModalEnvironment = () => {
+	return (
+		<Environment
+			files="https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/hdris/noon-grass/noon_grass_1k.hdr"
+			background
+		/>
+	);
+};
+
+export default ModalEnvironment;
--- src/components/SkillsModal/SkillsModal.tsx
+++ src/components/SkillsModal/SkillsModal.tsx
@@ -5,8 +5,9 @@ import { useCallback, useMemo } from "react";
 
 import BackToMainPageButton from "@/UI/BackToMainPageButton/BackToMainPageButton.tsx";
 
-import Cloud from "@/components/Cloud/Cloud.tsx";
+import Cloud1 from "@/components/Cloud/Cloud.tsx";
 import { justForFun, skills } from "@/components/SkillsModal/data";
+import ModalEnvironment from "@/components/SkillsModal/ModalEnvironment/ModalEnvironment.tsx";
 
 import useAppStore from "@/store/appSrore.ts";
 import createSelectors from "@/store/createSelectors.ts";
@@ -49,8 +50,9 @@ const SkillsModal = () => {
 
 			<Canvas dpr={[1, 2]} camera={{ position: [0, 0, 35], fov: 90 }}>
 				<fog attach="fog" args={["#27282c", 0, 80]} />
+				<ModalEnvironment />
 				<group rotation={[10, 10.5, 10]}>
-					<Cloud words={words} radius={20} />
+					<Cloud1 words={words} radius={20} />
 				</group>
 				<TrackballControls />
 			</Canvas>
