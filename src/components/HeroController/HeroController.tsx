abbb0dd7440f7053190cc9e296967fe7c95a177a
itolog <paukanlove@gmail.com>
2024-06-27 16:51:17 +0300

    fixed mode

--- src/components/HeroController/HeroController.tsx
+++ src/components/HeroController/HeroController.tsx
@@ -1,5 +1,8 @@
+import { useMemo } from "react";
+
 import { heroUrl } from "@/config";
 import { RigidItem } from "@/constants";
+import { hasTouchSupport } from "@/utils/bowser.ts";
 import Ecctrl, { EcctrlAnimation } from "ecctrl";
 
 import Hero from "@/components/Hero/Hero.tsx";
@@ -14,6 +17,10 @@ const animationSet = {
 };
 
 const HeroController = () => {
+	const mode = useMemo(() => {
+		return hasTouchSupport() ? undefined : "FixedCamera";
+	}, []);
+
 	return (
 		<Ecctrl
 			name={RigidItem.HERO_CONTROLLER}
@@ -23,6 +30,7 @@ const HeroController = () => {
 			camTargetPos={{ x: 0, y: 4, z: -1 }}
 			camInitDis={-7}
 			fixedCamRotMult={0.8}
+			mode={mode}
 			position={[-100, 0, 20]}
 			animated>
 			<EcctrlAnimation characterURL={heroUrl} animationSet={animationSet}>
