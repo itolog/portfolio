f2d7b95c3a386e29319d7307297fee6cae10b10b
itolog <paukanlove@gmail.com>
2024-07-10 13:41:44 +0300

    fix ts errors

--- src/components/VideoMaterial/VideoMaterial.tsx
+++ src/components/VideoMaterial/VideoMaterial.tsx
@@ -3,7 +3,7 @@ import { useVideoTexture } from "@react-three/drei";
 import { videoUrl } from "@/config";
 
 const VideoMaterial = () => {
-	const texture = useVideoTexture(videoUrl);
+	const texture = useVideoTexture(videoUrl as string);
 
 	return <meshStandardMaterial map={texture} toneMapped={false} />;
 };
