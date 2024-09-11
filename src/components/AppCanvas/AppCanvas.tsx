6b25c5bb4201e9edd9ec93a6603e6394922f8e0f
itolog <paukanlove@gmail.com>
2024-09-11 13:40:02 +0300

    up deps

--- src/components/AppCanvas/AppCanvas.tsx
+++ src/components/AppCanvas/AppCanvas.tsx
@@ -1,5 +1,4 @@
 import { Canvas } from "@react-three/fiber";
-import { CanvasProps } from "@react-three/fiber/dist/declarations/src/web/Canvas";
 import { FC, PropsWithChildren, Suspense } from "react";
 
 import Loader from "@/UI/Loader/Loader.tsx";
@@ -8,9 +7,7 @@ import Joystick from "@/components/Joystick/Joystick.tsx";
 import PoHint from "@/components/PoHint/PoHint.tsx";
 import SkillsModal from "@/components/Skills/components/SkillsModal/SkillsModal.tsx";
 
-interface Props extends CanvasProps {}
-
-const AppCanvas: FC<PropsWithChildren<Props>> = ({ children }) => {
+const AppCanvas: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<Suspense fallback={<Loader />}>
 			<Joystick />
