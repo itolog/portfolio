fc9d16d98c0bb25fce44d72ce0f5ad729dc85e94
itolog <paukanlove@gmail.com>
2024-10-30 15:42:59 +0200

    analytics

--- vite.config.ts
+++ vite.config.ts
@@ -2,41 +2,43 @@ import react from "@vitejs/plugin-react-swc";
 
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import eslintPlugin from "vite-plugin-eslint";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
+import eslintPlugin from "vite-plugin-eslint2";
 import oxlintPlugin from "vite-plugin-oxlint";
+import { VitePluginRadar } from "vite-plugin-radar";
 import stylelint from "vite-plugin-stylelint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		oxlintPlugin({
-			path: "src",
-		}),
-		eslintPlugin(),
-		stylelint({
-			fix: true,
-			lintInWorker: true,
-			cache: false,
-		}),
-	],
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "./src"),
-			"@components": resolve(__dirname, "./src/components"),
-			"@assets": resolve(__dirname, "./src/assets"),
-			"@layouts": resolve(__dirname, "./src/layouts"),
-			"@store": resolve(__dirname, "./src/store"),
-			"@pages": resolve(__dirname, "./src/pages"),
-			"@router": resolve(__dirname, "./src/router"),
-			"@styles": resolve(__dirname, "./src/styles"),
-			"@ui": resolve(__dirname, "./src/UI"),
-			"@types": resolve(__dirname, "./src/types"),
-			"@constants": resolve(__dirname, "./src/constants"),
-		},
-	},
+  plugins: [
+    react(),
+    oxlintPlugin({
+      path: "src",
+    }),
+    eslintPlugin(),
+    stylelint({
+      fix: true,
+      lintInWorker: true,
+      cache: false,
+    }),
+    VitePluginRadar({
+      analytics: {
+        id: process.env.VITE_G_TAG || "",
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@assets": resolve(__dirname, "./src/assets"),
+      "@layouts": resolve(__dirname, "./src/layouts"),
+      "@store": resolve(__dirname, "./src/store"),
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@router": resolve(__dirname, "./src/router"),
+      "@styles": resolve(__dirname, "./src/styles"),
+      "@ui": resolve(__dirname, "./src/UI"),
+      "@types": resolve(__dirname, "./src/types"),
+      "@constants": resolve(__dirname, "./src/constants"),
+    },
+  },
 });
